1c2c64a425cde200e8b42b6e5c68f98ebe9f785b
abdullahi0520 <90243393+abdullahi0520@users.noreply.github.com>
2022-02-07 16:58:17 -0600

    did code grade mvp

--- api/server.js
+++ api/server.js
@@ -13,16 +13,16 @@ server.get('/', (req,res) => {
 server.post('/api/users', (req,res)=> {
     let body = req.body;
     if (!body.name) {
-        res.status(400).json({message: `name is required`});
+        res.status(400).json({message: "Please provide name and bio for the user"});
     } else if (!body.bio) {
-        res.status(400).json({message: `bio is required`});
+        res.status(400).json({message: "Please provide name and bio for the user"});
     } else {
         model.insert(body)
             .then(users => {
                 res.status(201).json(users);
             })
             .catch(()=> {
-                res.status(500).json({message: `could not create user`});
+                res.status(500).json({message: "There was an error while saving the user to the database"});
             })
     }
 })
@@ -43,12 +43,12 @@ server.get('/api/users/:id', (req,res)=> {
         .then(users => {
             console.log(users);
             if(users == null) {
-                res.status(404).json({ message: `user ${id} not found!` });
+                res.status(404).json({ message: "The user with the specified ID does not exist" });
             } else {
                 res.json(users);
             }
         }).catch(()=> {
-            res.status(500).json({message: `could not get a dog`});
+            res.status(500).json({message: "The user information could not be retrieved"});
         })
 })
 server.delete('/api/users/:id', (req,res)=> {
@@ -56,13 +56,13 @@ server.delete('/api/users/:id', (req,res)=> {
     model.remove(id)
     .then(users => {
         if(users == null) {
-            res.status(404).json({ message: `user ${id} not found!` });  
+            res.status(404).json({ message: `The user with the specified ID does not exist` });  
             return;
         }
         res.status(200).json(users)
     })
     .catch(()=> {
-        res.status(500).json({message: 'couldnt not delete user'})
+        res.status(500).json({message: 'The user could not be removed'})
     })
 })
 
@@ -71,22 +71,22 @@ server.put('/api/users/:id', async (req, res) => {
     try {
     let users = await model.findById(id);
     if(users == null) {
-        res.status(404).json({message: `user ${id} not found!`});
+        res.status(404).json({message: "The user with the specified ID does not exist"});
         return;
     }
 
     let body = req.body;
     if (!body.name) {
-        res.status(500).json({message: `name is required`});
+        res.status(400).json({message: "Please provide name and bio for the user"});
     } else if (!body.bio) {
-        res.status(500).json({message: `bio is required`});
+        res.status(400).json({message: "Please provide name and bio for the user"});
     } else {
         let newUser = await model.update(id, body);
         console.log(newUser);
         res.status(200).json(newUser)
     }
 } catch(e) {
-    res.status(500).json({ message: `could not update dog!` });
+    res.status(500).json({ message: 'The user information could not be modified' });
 }
 });
 module.exports = server; // EXPORT YOUR SERVER instead of {}
